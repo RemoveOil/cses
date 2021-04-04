b7f455dad221fd7817d31a6b81bc16675541e36d
norubai <orbay.altuntoprak@gmail.com>
2021-04-04 23:49:58 +0100

    canary file

--- graphs/building_teams.cpp
+++ graphs/building_teams.cpp
@@ -1,37 +1,12 @@
 #include <iostream>
 #include <vector>
-// #include "../_utils.cpp"
+#include "../_utils.cpp"
+#include "../structures/unweighted_graph.cpp"
 
 using namespace std;
 
 using ulli = unsigned long long int;
 using lli = long long int;
-
-struct UWGraph {
-    vector<vector<int>> adj;
-    int N, M;
-    bool directed;
-
-    UWGraph(istream &stream, bool _directed = false) {
-
-        // vector<T> values;
-        directed = _directed;
-        stream >> N >> M;
-        adj.resize(N+1); // N or N + 1
-        // adj.size()  <--- bu guvenilir bi sey degil
-        for (int i = 0, src, dst; i < M; ++i) {
-            stream >> src >> dst; 
-            adj[src].push_back(dst);
-            if (!directed)  adj[dst].push_back(src);
-        }
-    }
-    
-    void add_edge(int src, int dst) {
-        adj[src].push_back(dst);
-        if (!directed)  adj[dst].push_back(src);
-        ++M;
-    }
-};
 
 vector<int> el_colors;
 bool sictik = false;
