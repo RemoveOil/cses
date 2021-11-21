c1e94752c2a3304c21eb25513c3cd0fed0151ac3
Safa Andaç <8544658+SanD94@users.noreply.github.com>
2021-11-21 13:35:55 +0300

    Geometry problems (#18)
    Co-authored-by: norubai <orbay.altuntoprak@gmail.com>

---
+++ geometry/line_segment_intersection.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cmath>
+#include "../_utils.cpp"
+
+using namespace std;
+using lli = long long int;
+
+struct Point {
+    lli x, y;
+    Point(lli _x, lli _y): x(_x), y(_y) {}
+    Point(): x(0), y(0) {}
+
+
+    Point operator-(const Point& other) const {
+        return Point(x - other.x, y - other.y);
+    }
+
+    friend istream& operator>>(istream& is, Point& p) {
+        is >> p.x >> p.y;
+        return is;
+    }
+
+    double dist_to_origin() {
+        return sqrt(x * x + y * y);
+    }
+};
+
+lli calc_det(Point x, Point y) {
+    return (x.x * y.y - x.y * y.x);
+    // -1/0/1
+}
+
+int orientation(Point x, Point y, Point z) {
+    // x,y dogru. z nokta.
+    lli det = calc_det(y - x, z - x);
+    if (det > 0)    return 1;
+    if (det < 0)    return -1;
+    return 0;
+}
+
+bool in_between(const Point& q1, const Point& q2, const Point& w1) {
+    double dst1 = (q2-q1).dist_to_origin();
+    double dst2 = (w1-q1).dist_to_origin();
+    double dst3 = (w1-q2).dist_to_origin();
+    return !(dst2 + dst3 > dst1);
+}
+
+
+
+int run_test() {
+    Point q1, q2, w1, w2;
+    cin >> q1 >> q2 >> w1 >> w2;
+    int o_q1q2w1 = orientation(q1, q2, w1);
+    int o_q1q2w2 = orientation(q1, q2, w2);
+    int o_w1w2q1 = orientation(w1, w2, q1);
+    int o_w1w2q2 = orientation(w1, w2, q2);
+
+    // double oh
+    if (!o_q1q2w1 and !o_q1q2w2)
+        return (
+            in_between(q1, q2, w1)
+            or in_between(q1, q2, w2)
+            or in_between(w1, w2, q1)
+            or in_between(w1, w2, q2));
+    // LL/RR
+    if ((o_q1q2w1 == o_q1q2w2) or (o_w1w2q1 == o_w1w2q2))
+        return false;
+
+    // YUKSEK TESTOSTERON BI NOTASYON.
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while(t--)
+        cout << (run_test() ? "YES" : "NO") << endl;
+
+
+
+    return 0;
+}
